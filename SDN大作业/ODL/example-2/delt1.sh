0cd7f0fcdab9abc828c5bc1e301f53f98e997f78
snow-shadow <95010715+snow-shadow@users.noreply.github.com>
2021-11-30 19:55:59 +0800

    Add files via upload

---
+++ SDN大作业/ODL/example-2/delt1.sh
@@ -0,0 +1,3 @@
+sudo ovs-ofctl -O Openflow13 del-flows c1
+sudo ovs-ofctl -O Openflow13 del-flows a3
+sudo ovs-ofctl -O Openflow13 del-flows a5
---
+++ SDN大作业/ODL/example-2/delt2.sh
@@ -0,0 +1,3 @@
+sudo ovs-ofctl -O Openflow13 del-flows c2
+sudo ovs-ofctl -O Openflow13 del-flows a4
+sudo ovs-ofctl -O Openflow13 del-flows a6
