2af39ac11f42baaaf8561182e627ab774aa6a0b7
snow-shadow <95010715+snow-shadow@users.noreply.github.com>
2021-11-30 20:04:24 +0800

    Delete SDN大作业 directory

--- SDN大作业/ODL/example-2/delt1.sh
+++
@@ -1,3 +0,0 @@
-sudo ovs-ofctl -O Openflow13 del-flows c1
-sudo ovs-ofctl -O Openflow13 del-flows a3
-sudo ovs-ofctl -O Openflow13 del-flows a5
--- SDN大作业/ODL/example-2/delt2.sh
+++
@@ -1,3 +0,0 @@
-sudo ovs-ofctl -O Openflow13 del-flows c2
-sudo ovs-ofctl -O Openflow13 del-flows a4
-sudo ovs-ofctl -O Openflow13 del-flows a6
